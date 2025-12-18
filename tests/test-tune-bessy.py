deb8a82bf343d6a3ae25b19ea88785023c93abb0
PONS <pons@esrf.fr>
2025-12-18 11:20:49 +0100

    Added bessy tune correction

---
+++ tests/test-tune-bessy.py
@@ -0,0 +1,25 @@
+from pyaml.accelerator import Accelerator
+from pyaml.magnet.magnet import Magnet
+from pyaml.common.constants import ACTION_RESTORE
+
+import numpy as np
+import time
+
+def tune_callback(step: int, action: int, m: Magnet, dtune: np.array):
+    if action == ACTION_RESTORE:
+        # On action restore, the delta tune is passed as argument
+        print(f"Tune response: #{step} {m.get_name()} {dtune}")
+    return True
+
+sr = Accelerator.load("bessy2tune.yaml")
+
+#sr.design.get_lattice().disable_6d()
+#tune_adjust = sr.design.tune
+#tune_adjust.response.measure(callback=tune_callback)
+#tune_adjust.response.save_json("tunemat-bessy.json")
+
+sr.live.tune.response.load_json("tunemat-bessy.json")
+print(sr.live.tune.readback())
+sr.live.tune.set([0.83, 0.84],iter=3,wait_time=5)
+print(sr.live.tune.readback())
+
