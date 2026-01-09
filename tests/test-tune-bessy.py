3afa65f83ef241a1249841731fc7cb627a30ae40
PONS <pons@esrf.fr>
2026-01-09 11:08:02 +0100

    Move BESSY2 yaml to KL convention

--- tests/test-tune-bessy.py
+++ tests/test-tune-bessy.py
@@ -6,27 +6,17 @@ import numpy as np
 import time
 
 
-
-
 def tune_callback(step: int, action: int, m: Magnet, dtune: np.array):
     if action == ACTION_RESTORE:
-        # On action restore, the delta tune is passed as argument
+        # On action restore, the measured delta tune is passed as argument
         print(f"Tune response: #{step} {m.get_name()} {dtune}")
     return True
 
-sr = Accelerator.load("bessy2tune.yaml")
-print(sr.live.get_magnet("Q3MD1R").strength.get())
-print(sr.design.get_magnet("Q3MD1R").strength.get())
+sr = Accelerator.load("bessy2tune-KL.yaml")
 
-print(sr.live.get_magnet("Q3MD1R").model.get_devices()[0])
-print(sr.live.get_magnet("Q3MD1R").strength._RWStrengthScalar__dev)
-
-ring = sr.design.get_lattice()
-for e in ring:
-    if e.FamName in ["Q3M1D1R","Q3M2D1R"]:
-        print(e)
-quit()
-
+#print(sr.live.get_magnets("QForTune").strengths.get())
+#print(sr.design.get_magnets("QForTune").strengths.get())
+#quit()
 
 #sr.design.get_lattice().disable_6d()
 #tune_adjust = sr.design.tune
@@ -35,6 +25,7 @@ quit()
 
 sr.live.tune.response.load_json("tunemat-bessy.json")
 print(sr.live.tune.readback())
-sr.live.tune.set([0.83, 0.84],iter=10,wait_time=5)
+sr.live.tune.set([0.83, 0.84],iter=2,wait_time=3)
+time.sleep(3)
 print(sr.live.tune.readback())
 
