4548aa03e3d3ba4d231d631797b6b6672cf3a74b
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2026-04-22 13:53:33 +0200

    Adapt OA control system to new pyaml API

--- pyaml_cs_oa/controlsystem.py
+++ pyaml_cs_oa/controlsystem.py
@@ -1,12 +1,11 @@
-import os
 import logging
-import copy
 
-from pyaml.control.controlsystem import ControlSystem
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from pyaml.common.exception import PyAMLException
+from pyaml.configuration.catalog import Catalog
+from pyaml.control.controlsystem import ControlSystem
 
-PYAMLCLASS : str = "OphydAsyncControlSystem"
+PYAMLCLASS: str = "OphydAsyncControlSystem"
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +26,6 @@ from .tangoRW import TangoRW
 from . import __version__
 
 class ConfigModel(BaseModel):
-
     """
     Configuration model for an OA Control System.
 
@@ -36,21 +34,26 @@ class ConfigModel(BaseModel):
     name : str
         Name of the control system.
     prefix : str
-        Prefix added to the PV or attribute name. It can be a 
+        Prefix added to the PV or attribute name. It can be a
         for instance, TANGO_HOST, or a PV prefix.
-    debug_level : int
+    catalog : Catalog | str | None
+        Catalog instance or catalog name used to resolve PyAML device keys.
+    debug_level : str
         Debug verbosity level.
     scalar_aggregator : str
-        Aggregator module for scalar values. If none specified, writings and 
-        readings of sclar value are serialized. 
+        Aggregator module for scalar values. If none specified, writings and
+        readings of scalar values are serialized.
     vector_aggregator : str
         Aggregator module for vecrors. If none specified, writings and readings
         of vector are serialized,
     """
 
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="forbid")
+
     name: str
     prefix: str = ""
-    debug_level: str=None
+    catalog: Catalog | str | None = None
+    debug_level: str | None = None
     scalar_aggregator: str | None = "pyaml_cs_oa.scalar_aggregator"
     vector_aggregator: str | None = None
 
@@ -61,48 +64,53 @@ class OphydAsyncControlSystem(ControlSystem):
     def __init__(self, cfg: ConfigModel):
         super().__init__()
         self._cfg = cfg
-        self._devices = {} # Dict containing all attached DeviceAccess
+        self._devices = {}  # Dict containing all attached DeviceAccess
 
         if self._cfg.debug_level:
-          log_level = getattr(logging, self._cfg.debug_level, logging.WARNING)
-          logger.parent.setLevel(log_level)
-          logger.setLevel(log_level)
+            log_level = getattr(logging, self._cfg.debug_level, logging.WARNING)
+            logger.parent.setLevel(log_level)
+            logger.setLevel(log_level)
 
-        logger.log(logging.WARNING, f"PyAML OA control system binding ({__version__}) initialized with name '{self._cfg.name}'"
-                                 f" and prefix='{self._cfg.prefix}'")
-        
-    def attach(self, devs: list[OASignal]) -> list[OASignal]:
-        return self._attach(devs,False)
+        logger.log(
+            logging.WARNING,
+            f"PyAML OA control system binding ({__version__}) initialized with name '{self._cfg.name}'"
+            f" and prefix='{self._cfg.prefix}'",
+        )
 
-    def attach_array(self, devs: list[OASignal]) -> list[OASignal]:
-        return self._attach(devs,True)
+    def attach(self, devs: list[OASignal | None]) -> list[OASignal | None]:
+        return self._attach(devs, False)
 
-    def _attach(self, devs: list[OASignal],is_array:bool) -> list[OASignal]:
+    def attach_array(self, devs: list[OASignal | None]) -> list[OASignal | None]:
+        return self._attach(devs, True)
+
+    def _attach(self, devs: list[OASignal | None], is_array: bool) -> list[OASignal | None]:
         # Concatenate the prefix
         newDevs = []
-        for d in devs:            
+        for d in devs:
             if d is not None:
-
                 sig_cfg = d._cfg
                 sig_cfg_cls = sig_cfg.__class__
-                
-                if isinstance(d._cfg,EpicsConfigR):
+
+                if isinstance(d._cfg, EpicsConfigR):
                     key = self._cfg.prefix + d._cfg.read_pvname
                     sig_cls = EpicsR
                     config = dict(read_pvname=key)
-                elif isinstance(d._cfg,EpicsConfigW):
+                elif isinstance(d._cfg, EpicsConfigW):
                     key = self._cfg.prefix + d._cfg.write_pvname
                     sig_cls = EpicsW
                     config = dict(write_pvname=key)
-                elif isinstance(d._cfg,EpicsConfigRW):
+                elif isinstance(d._cfg, EpicsConfigRW):
                     key = self._cfg.prefix + d._cfg.read_pvname + d._cfg.write_pvname
                     sig_cls = EpicsRW
-                    config = dict(read_pvname=self._cfg.prefix + d._cfg.read_pvname, write_pvname=self._cfg.prefix + d._cfg.write_pvname)
-                elif isinstance(d._cfg,TangoConfigR):
+                    config = dict(
+                        read_pvname=self._cfg.prefix + d._cfg.read_pvname,
+                        write_pvname=self._cfg.prefix + d._cfg.write_pvname,
+                    )
+                elif isinstance(d._cfg, TangoConfigR):
                     key = self._cfg.prefix + d._cfg.attribute
                     sig_cls = TangoR
                     config = dict(attribute=key)
-                elif isinstance(d._cfg,TangoConfigRW):
+                elif isinstance(d._cfg, TangoConfigRW):
                     key = self._cfg.prefix + d._cfg.attribute
                     sig_cls = TangoRW
                     config = dict(attribute=key)
@@ -110,8 +118,8 @@ class OphydAsyncControlSystem(ControlSystem):
                     raise PyAMLException(f"OphydAsyncControlSystem: Unsupported type {type(sig_cfg)}")
 
                 if key not in self._devices:
-                    n_conf =  dict(d._cfg) | config
-                    nr = sig_cls(sig_cfg_cls(**n_conf),is_array)
+                    n_conf = dict(d._cfg) | config
+                    nr = sig_cls(sig_cfg_cls(**n_conf), is_array)
                     nr.build()
                     self._devices[key] = nr
 
@@ -130,7 +138,7 @@ class OphydAsyncControlSystem(ControlSystem):
             Name of the control system.
         """
         return self._cfg.name
-    
+
     def scalar_aggregator(self) -> str | None:
         """
         Returns the module name used for handling aggregator of DeviceAccess
@@ -154,4 +162,4 @@ class OphydAsyncControlSystem(ControlSystem):
         return self._cfg.vector_aggregator
 
     def __repr__(self):
-       return repr(self._cfg).replace("ConfigModel",self.__class__.__name__)
+        return repr(self._cfg).replace("ConfigModel", self.__class__.__name__)
